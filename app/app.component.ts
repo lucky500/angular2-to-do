fe451bd33e7c2a4608c1c9ebf7f2d226e3d48bfb
Guest User <Guest@epicodus-5.local>
2017-10-25 10:40:35 -0700

    add ngIf to toggle edit form

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,26 +7,68 @@ import { Component } from '@angular/core';
     <h1>To Do App {{month}}/{{day}}/{{year}}</h1>
     <h4>{{currentFocus}}</h4>
     <ul>
-      <li *ngFor="let currentTask of tasks">{{currentTask.description}}</li>
+      <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}}
+      <button (click)="editTask(currentTask)">Edit</button></li>
     </ul>
+    <hr>
+    <div *ngIf="selectedTask">
+      <h3>{{selectedTask.description}}</h3>
+      <p>Task Complete? {{selectedTask.done}}</p>
+      <h3>Edit Task</h3>
+      <label>Enter Task Desciption:</label>
+      <input [(ngModel)]="selectedTask.description">
+      <label>Enter Task Priority (1-3):</label>
+      <br>
+      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
+      <input type="radio"[(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
+      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3"> 3 (High Priority)
+      <button (click)="finishedEditing()">Done</button>
   </div>
   `
 })
 
-export class AppComponent { 
+export class AppComponent {
   currentFocus: string = "Learn Angular 2";
   currentTime = new Date();
   month: number  = this.currentTime.getMonth() + 1;
   day:   number  = this.currentTime.getDate();
   year:  number  = this.currentTime.getFullYear();
   tasks: Task[] = [
-    new Task("Do Angular2 Tour of Heros Example"),
-    new Task('Begin brainstorming possible JavaScript group projects'),
-    new Task('Add README file to last few Angular repos on GitHub') 
+    new Task("Do Angular2 Tour of Heros Example", 3),
+    new Task('Begin brainstorming possible JavaScript group projects', 2),
+    new Task('Add README file to last few Angular repos on GitHub', 2)
   ];
+
+  selectedTask = null;
+
+  editTask(clickedTask){
+    this.selectedTask = clickedTask;
+  }
+
+  isDone(clickedTask: Task){
+    if(clickedTask.done === true){
+      alert("This task is done!");
+    } else {
+      alert("This task is not done. Better get to work!");
+    }
+  }
+
+  priorityColor(currentTask){
+    if (currentTask.priority === 3){
+      return "bg-danger";
+    } else if (currentTask.priority === 2){
+      return "bg-warning";
+    } else {
+      return "bg-info";
+    }
+  }
+
+  finishedEditing(){
+    this.selectedTask = null;
+  }
 }
 
 export class Task {
   public done: boolean = false;
-  constructor(public description: string){}
+  constructor(public description: string, public priority: number){}
 }
